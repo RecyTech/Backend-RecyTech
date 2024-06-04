909bce274c9664034501326c4eeaa46bfffaa493
Salvlormonch1 <salvlormonch@gmail.com>
2024-06-04 11:14:14 -0500

    feat: Created shared packages and files.

---
+++ src/main/java/com/acme/recytechbackend/RecyTechBackendApplication.java
@@ -0,0 +1,13 @@
+package com.acme.recytechbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecyTechBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecyTechBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/recytechbackend/RecyTechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.recytechbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecyTechBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
